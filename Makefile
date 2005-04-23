c7f3b5680c65be6becdc4f9ff7d6eb230a3d3880
hachi <hachi@82996608-69f5-0310-b8a8-91ba82a82013>
2005-04-23 12:12:24 +0000

    r37@yuumi:  hachi | 2005-04-23 07:05:11 -0500  Prepare everything for installation-style usage
    git-svn-id: svn+ssh://kuiki.net/home/hachi/lib/svn/cd-backup/trunk@5 82996608-69f5-0310-b8a8-91ba82a82013

---
+++ Makefile
@@ -0,0 +1,9 @@
+BASE	?= /usr/local
+PREFIX	= $(DESTDIR)$(BASE)
+BIN	= $(PREFIX)/bin
+
+install:	cd-backup
+		install	./cd-backup $(BIN)
+
+diff-install:
+	diff -u $(BIN)/cd-backup ./cd-backup || true
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 cd /data/hidden/backups/cd-backup
-./backup.pl 600 kuiki.net '/data/hidden/backups/rosa/www-backup=www' '/data/hidden/backups/rosa/mysql.dump=mysql.dump' '/home=home'
+cd-backup 600 kuiki.net '/data/hidden/backups/rosa/www-backup=www' '/data/hidden/backups/rosa/mysql.dump=mysql.dump' '/home=home'
 result=$?
 cdrecord -dao -eject dev=/dev/hdc driveropts=burnfree kuiki.net.iso
 echo $result
